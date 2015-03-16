442c3cd94456edc118cd1e7eed459ee3cea19f3e
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-03-16 18:04:05 +0100

    added changelog entry

--- src/eu/nethazard/yt/Config.java
+++ src/eu/nethazard/yt/Config.java
@@ -2,6 +2,7 @@
  * @author Mathias Dickenscheid
  * @since 2014-09-07
  * 
+ * 2015-03-16 1.0.3 added missing itags
  * 2015-03-16 1.0.2 changed how the file name is cleaned
  * 2015-03-16 1.0.1 fix: single 'Representation' entries don't throw an exception anymore
  * 2014-09-07 1.0.0 initial release
@@ -17,7 +18,7 @@ public class Config {
 	public static boolean REMOVE_TRACKS_AFTER_MUXING = true;
 	
 	private final static String guiVersion = "1.0.0";
-	private final static String libraryVersion = "1.0.2";
+	private final static String libraryVersion = "1.0.3";
 	
 	public static String getGUIVersion(){
 		return guiVersion;
