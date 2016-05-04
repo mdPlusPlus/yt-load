e49171cbfe7fb9416eaa5eac917c1bb935824b2c
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2016-05-04 03:18:37 +0200

    added version info

--- src/eu/nethazard/yt/gui/ConfigGUI.java
+++ src/eu/nethazard/yt/gui/ConfigGUI.java
@@ -4,9 +4,9 @@ package eu.nethazard.yt.gui;
  * @author Mathias Dickenscheid
  * @since 2014-09-07
  *
- * xxxx-xx-xx 1.1.0 added itag dropdown selection
- * 2015-11-26 1.2.0 disable GUI elements while downloading
  * 2015-11-28 1.3.0 open explorer on double-click
+ * 2015-11-26 1.2.0 disable GUI elements while downloading
+ * xxxx-xx-xx 1.1.0 added itag dropdown selection
  */
 public class ConfigGUI {
 
